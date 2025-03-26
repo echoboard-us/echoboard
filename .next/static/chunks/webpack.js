4f38f685e74c7ca2aa55017332defe5491a2d037
trinayhari <trinayhari@gmail.com>
2025-03-26 17:16:01 -0400

    edits

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ad0a2edc33568177"; }
+/******/ 		__webpack_require__.h = function() { return "1064be15b5bc4b7e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/surveys/page.tsx
+++ src/app/surveys/page.tsx
@@ -4,7 +4,7 @@ export default function SurveysPage() {
   return (
     <Card className="mt-6">
       <Title>Surveys</Title>
-      <Text className="mt-4">Coming soon...</Text>
+      <Text className="mt-4">Coming soon....</Text>
     </Card>
   );
 }
